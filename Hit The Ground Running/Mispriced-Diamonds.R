b064b3ebea8cd33a64683c6e35108c56ad6f3d12
cyberdud3 <abhijith@cyberdude.com>
2020-12-01 01:24:58 +0530

    smoothened the curve and found the mispricings

--- Hit The Ground Running/Mispriced-Diamonds.R
+++ Hit The Ground Running/Mispriced-Diamonds.R
@@ -5,4 +5,5 @@ install.packages("ggplot2")
 
 ggplot(data = myData[myData$carat<2.5,], 
        aes(x = carat, y = price, colour = clarity)) +
-   geom_point(alpha = 0.1)
+   geom_point(alpha = 0.1) +
+   geom_smooth()
