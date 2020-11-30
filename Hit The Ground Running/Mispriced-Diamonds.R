d99904be281cbb3091dafe6cdcf5ac5410f31895
cyberdud3 <abhijith@cyberdude.com>
2020-12-01 01:01:18 +0530

    clarity is added as a parameter to distinguish the clarity variants

--- Hit The Ground Running/Mispriced-Diamonds.R
+++ Hit The Ground Running/Mispriced-Diamonds.R
@@ -3,5 +3,6 @@ myData <- read.csv(file.choose())
 
 install.packages("ggplot2")
 
-ggplot(data = myData, aes(x = carat, y = price)) +
+ggplot(data = myData, 
+       aes(x = carat, y = price, colour = clarity)) +
    geom_point()
