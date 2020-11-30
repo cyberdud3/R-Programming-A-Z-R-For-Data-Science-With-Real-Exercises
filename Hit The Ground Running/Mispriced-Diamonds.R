7be955db2945fd411f32812f83ec17a32fc8f566
cyberdud3 <abhijith@cyberdude.com>
2020-12-01 01:22:58 +0530

    filtered the insignificant points

--- Hit The Ground Running/Mispriced-Diamonds.R
+++ Hit The Ground Running/Mispriced-Diamonds.R
@@ -3,6 +3,6 @@ myData <- read.csv(file.choose())
 
 install.packages("ggplot2")
 
-ggplot(data = myData, 
+ggplot(data = myData[myData$carat<2.5,], 
        aes(x = carat, y = price, colour = clarity)) +
    geom_point(alpha = 0.1)
