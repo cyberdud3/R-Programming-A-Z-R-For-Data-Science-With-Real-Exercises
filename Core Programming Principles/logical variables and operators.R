2b64b32a2ea42af11f1865fcdefc83ed2f246e76
cyberdud3 <abhijith@cyberdude.com>
2020-12-03 10:10:22 +0530

    various operations with logical and relational operators

---
+++ Core Programming Principles/logical variables and operators.R
@@ -0,0 +1,35 @@
+#TRUE T
+#FALSE F
+
+4 < 5
+10 > 100 
+
+# == is equal to or comparison operator
+4 == 10
+
+# != not equal to or comparison operator
+5 != 4
+5 != 5
+
+# < less than <= less than or equal to
+# > >=
+
+result1 <- 5 > 2
+result2 <- 7 < 3
+
+# ! logical not
+!result1
+r1 <- !result1
+
+# | logical or
+result1 | result2
+r2 <- result1 | result2
+
+# & logical and
+result1 & result2
+r3 <- result1 & result2
+
+# isTRUE(any condition)
+isTRUE(r3)
+isTRUE(r2)
+
