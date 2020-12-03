c00b4380030d69778470d43f8088d362a828efb9
cyberdud3 <abhijith@cyberdude.com>
2020-12-03 08:46:33 +0530

    r supported variable name combinations

---
+++ Core Programming Principles/valid_variable_names.R
@@ -0,0 +1,20 @@
+
+#valid
+Full_name
+
+#valid
+Full_name.
+
+#invalid - starts with a number
+2Full_name
+
+#valid
+.Full_name
+Full.name
+._Full.name
+
+#invalid - dot is preceeded by number
+.2Full_name
+
+#invalid - starts with _
+_Full_name
