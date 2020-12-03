afc272fe5e777585d886fa5f834682689afdf476
cyberdud3 <abhijith@cyberdude.com>
2020-12-03 09:54:42 +0530

    various uses of variables discussed

---
+++ Core Programming Principles/use of variables.R
@@ -0,0 +1,29 @@
+A <- 10
+B <- 5
+
+C <- A + B
+
+#to print value directly
+#it is not necessary to type print
+#all the time
+C
+
+#variable 1
+var1 <- 3.33
+
+#variable 2
+var2 <- 2
+
+result <- var1 + var2
+
+
+var3 <- 4
+result1 <- sqrt(var3)
+result1
+
+result2 <- sqrt(9)
+result2
+
+greeting <- "Hi!"
+name <- "Liji"
+message <- paste(greeting, name)
