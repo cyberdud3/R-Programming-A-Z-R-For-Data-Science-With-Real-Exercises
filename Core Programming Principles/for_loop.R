41f32c66b4b2be522d5d49f517e907c2e3570806
cyberdud3 <abhijith@cyberdude.com>
2020-12-08 13:39:27 +0530

    for loop concept explained

---
+++ Core Programming Principles/for_loop.R
@@ -0,0 +1,18 @@
+counter <- 1
+while (counter < 5) { 
+   print("Hi Liji!")
+   counter <- counter + 1
+}
+
+############################
+
+for (i in 1:5) {
+   print("Hi Liji!")
+}
+
+############################
+
+for (i in 5:10) {
+   print("Hi Liji!")
+}
+
