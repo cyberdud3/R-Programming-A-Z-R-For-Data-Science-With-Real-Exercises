a6b2371e5d652671598e4e6c0004e475dee88054
cyberdud3 <abhijith@cyberdude.com>
2020-12-08 13:12:54 +0530

    while loop working explained

---
+++ Core Programming Principles/while_loop.R
@@ -0,0 +1,17 @@
+while (FALSE) { 
+   print("Hi Liji!")
+}
+
+##########################
+
+while (TRUE) { 
+   print("Hi Liji!")
+}
+
+##########################
+
+counter <- 1
+while(counter < 10) {
+   print(counter)
+   counter <- counter + 1
+}
