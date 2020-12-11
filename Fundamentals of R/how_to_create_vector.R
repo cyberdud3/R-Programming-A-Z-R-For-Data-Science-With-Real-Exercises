5c8b22f75a800ea26d438e391756d325e4539197
cyberdud3 <abhijith@cyberdude.com>
2020-12-11 10:02:35 +0530

    sequence and replication explained

---
+++ Fundamentals of R/how_to_create_vector.R
@@ -0,0 +1,22 @@
+#sequence - it is like ':'
+#replicate
+
+seq(1, 15)
+# 1  2  3  4  5  6  7  8  9 10 11 12 13 14 15
+
+seq(1, 15, 2)
+# 1  3  5  7  9 11 13 15
+
+z <- seq(1, 5)
+z
+
+rep(3, 50)
+r <- rep(3, 50)
+r
+
+rep("Hi!", 5)
+s <- rep("Hi!", 5)
+s
+
+x <- c(80, 5)
+y <- rep(x, 4)
